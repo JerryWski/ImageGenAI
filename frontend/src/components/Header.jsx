e71a6f9c19cc1c6ffc29589510aea4dd296694b2
JerryWski <jarek_wasilewski@yahoo.com>
2025-01-21 21:14:58 +0100

    working logout

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { useAuthContext } from '../store/auth-context';
+
+const Header = () => {
+  const { token, logout } = useAuthContext();
+  return (
+    <header className='text-center text-stone-50 p-3'>
+      <h1 className='font-mono font-bold text-[clamp(1rem,3vw+1rem,5rem)]'>
+        Image Generator
+      </h1>
+      {token && (
+        <button
+          onClick={logout}
+          className='mt-2 text-stone-200 hover:text-stone-400'
+        >
+          Logout
+        </button>
+      )}
+    </header>
+  );
+};
+
+export default Header;
