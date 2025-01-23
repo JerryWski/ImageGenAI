bbb4a504c4955d835c81e9fb8246fe7c7220ffa3
JerryWski <jarek_wasilewski@yahoo.com>
2025-01-23 22:53:45 +0100

    small adjustemnts

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -1,30 +1,25 @@
 const Gallery = () => {
-  // const images = [
-  //     '/frontend/src/assets/img1.webp',
-  //     '/frontend/src/assets/img1.webp',
-  //     '/frontend/src/assets/img1.webp',
-  //     '/frontend/src/assets/img1.webp',
-  //     '/frontend/src/assets/img1.webp',
-  //     '/frontend/src/assets/img1.webp',
-  //     '/frontend/src/assets/img1.webp',
-  //     '/frontend/src/assets/img1.webp',
-  // ]
+  const images = [
+    '/frontend/src/assets/img2.webp',
+    '/frontend/src/assets/img1.webp',
+    '/frontend/src/assets/img3.webp',
+    '/frontend/src/assets/img1.webp',
+    '/frontend/src/assets/img3.webp',
+    '/frontend/src/assets/img2.webp',
+    '/frontend/src/assets/img3.webp',
+    '/frontend/src/assets/img1.webp',
+    '/frontend/src/assets/img2.webp',
+    
+  ];
 
   return (
-    <div className='container mx-auto max-w-[1100px] px-4 py-8'>
+    <div className='container mx-auto max-w-[1100px] max-h-[700px] overflow-hidden px-4 py-8'>
       <h1 className='text-2xl font-bold text-center mb-6'>Masonry Gallery</h1>
 
       <div className='columns-2 xs:columns-1 sm:columns-3'>
-        <img className="w-full mb-4  " src='/frontend/src/assets/img2.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img1.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img3.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img1.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img3.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img2.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img3.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img1.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img2.webp' alt='' />
-        <img className="w-full mb-4  " src='/frontend/src/assets/img2.webp' alt='' />
+        {images.map((image, index) => (
+          <img key={index} className='w-full mb-4' src={image} alt='ai generated image' />
+        ))}
       </div>
     </div>
   );
