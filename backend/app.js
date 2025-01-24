bff1f1203094e2f850c1968bed20cbc967474b88
JerryWski <jarek_wasilewski@yahoo.com>
2025-01-24 23:11:37 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,27 @@ import dotenv from 'dotenv';
 import express from 'express';
 import { loginUser, createUser, enforceAuth } from './middleware/auth.js';
 import { generateImage } from './middleware/image.js';
-import cors from 'cors';
+// import cors from 'cors';
 
 const app = express();
 dotenv.config();
 
-const corsOptions = {
-  origin: ['http://localhost:10000', 'https://imagegenai-5hdx.onrender.com/'], // Domena frontendu
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ['http://localhost:10000', 'https://imagegenai-5hdx.onrender.com/'], // Domena frontendu
+//   methods: ['GET', 'POST', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// };
 
 
 app.use(express.json());
-app.use(cors(corsOptions));
+// 
+ap.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.post('/signup', async (req, res) => {
   try {
