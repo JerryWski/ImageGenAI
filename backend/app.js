5748a88544afc79d1125b4ac36e61e5a5d6791fa
JerryWski <jarek_wasilewski@yahoo.com>
2025-01-24 22:01:02 +0100

    corsv2

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
+  next()
 });
 
 app.use(express.json());
